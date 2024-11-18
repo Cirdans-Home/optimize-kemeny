868cc8664263ac749e73c0c2597ce9c5b9650fbb
Fabio Durastante <fabio.durastante@unipi.it>
2024-11-18 10:51:08 +0100

    Align preserving and hessian implementation.

--- optimizer/optimizekemeny.m
+++ optimizer/optimizekemeny.m
@@ -109,11 +109,6 @@ switch upper(p.Results.type)
     case 'PRESERVING'
         % Solver for dense matrix P enforcing preservation of the
         % stationary vector
-        if abs(sum(p.Results.h) - 1) > 10*eps
-            h = p.Results.h./sum(p.Results.h);
-        else
-            h = p.Results.h;
-        end
         if size(P,1) ~= length(p.Results.stationary)
             error('OptimizeKemeny: stationary vector has wrong size %d',length(p.Results.stationary))
         end
@@ -125,7 +120,7 @@ switch upper(p.Results.type)
         else
             pi = p.Results.stationary;
         end
-        Delta = preserving_solver(P,pi,h,p.Results.verbose);
+        Delta = preserving_solver(P,pi,p.Results.verbose);
     case 'SPARSE'
         % Solver for obtaining a sparse perturbation without preservation
         % of the stationary vector
@@ -184,7 +179,7 @@ x = fmincon(objfun,x,[],[],Aeq,beq,lb,[],[],options);
 Delta = reshape(x,n,n);
 end
 
-function Delta = preserving_solver(P,pi,h,verbose)
+function Delta = preserving_solver(P,pi,verbose)
 %%UNSTRUCTURED_SOLVER Solve the optimization problem assuming P a dense
 %matrix and that the original stationary vector has to be preserved
 % Inizialize auxiliary quantities
@@ -203,13 +198,22 @@ Aeq = [kron(e',I);kron(I,pi');kron(I,Dpi) - kron(Dpi,I)*T];
 beq = [zeros(n,1);zeros(n,1);zeros(n^2,1)];
 % Inequality constraints can be expressed as lower-bounds
 lb = - P(:);
+if exist('assemble_hessian.mexa64','file') == 3
+    fprintf("Found Hessian mex file implementation.\n")
+    hess = @(Delta,lambda) assemble_H_mex(Delta,lambda,P,pi);
+else
+    fprintf("Using purely MATLAB Hessian.\n")
+    hess = @(Delta,lambda) assemble_H(Delta,lambda,P,pi);
+end
 options = optimoptions('fmincon','Algorithm','interior-point',...
     'SpecifyObjectiveGradient',true,...
-    'HessianApproximation','lbfgs',...
+    'HessianFcn',hess,...
     'SubproblemAlgorithm',solver,...
     'Display',verbose);
 x = zeros(n^2,1);
-objfun = @(Delta) objective(Delta,P,h);
+sqp = sqrt(pi);
+S = (sqp./sqp');
+objfun = @(Delta) objective_sym(Delta,P,pi,S);
 x = fmincon(objfun,x,[],[],Aeq,beq,lb,[],[],options);
 Delta = reshape(x,n,n);
 end
@@ -242,6 +246,8 @@ Delta = sparse(ival,jval,x,n,n);
 end
 
 %% Objective functions
+% These routines implement the different evaluation strategies for the
+% objective function
 
 function [f,g] = objective(Delta,P,h)
 %%OBJECTIVE Objective function containing Kemeny constant for a dense
@@ -284,6 +290,96 @@ if nargout > 1
     g = Gmat(index) + Delta;
 end
 end
+
+function [f,g] = objective_sym(Delta,P,pi,S)
+%%OBJECTIVE Objective function containing Kemeny constant for a dense
+% Markov chain.
+
+n = size(P,1);
+I = eye(n,n);
+
+Delta = reshape(Delta,n,n);
+sqp = sqrt(pi);
+Dp = spdiags(sqp,0,n,n);
+Dpinv = spdiags(1./sqp,0,n,n);
+
+Q = I - Dp*(P+Delta)*Dpinv + sqp*sqp';
+L = chol(Q);
+
+INV1 = L'\(L\I);
+
+f = trace( INV1 ) + 0.5*norm(Delta,"fro")^2;
+
+if nargout > 1
+    % Compute the gradient if it is requested
+    Gmat = S.*transpose(INV1*INV1);
+    g = Gmat(:) + Delta(:);
+end
+
+end
+
+%% Hessian Computation
+% These routines implement the computation of the Hessian matrix for the
+% optimization problem
+function H = assemble_H(Delta,lambda,P,pi)
+%ASSEMBLE_H builds the Hessian for the Kemeny's constant function
+
+n = size(P,1);
+I = eye(n,n);
+Delta = reshape(Delta,n,n);
+sqp = sqrt(pi);
+Dp = spdiags(sqp,0,n,n);
+Dpinv = spdiags(1./sqp,0,n,n);
+L = chol(I - Dp*(P+Delta)*Dpinv + sqp*sqp');
+INV1 = L'\(L\I);
+GMAT = INV1*INV1;
+
+% Define the dimension n
+n = size(INV1, 1);
+H = zeros(n^2, n^2);  % Initialize the matrix H of size n^2 x n^2
+
+% Nested loops to fill the H matrix
+for i=1:n
+    for j=1:n
+        p = i + (j-1)*n;
+        for h=1:n
+            for k=1:n
+                q = h + (k-1)*n;
+                term1 = INV1(j, k); % Compute e_j' * INV1 * e_h
+                term2 = GMAT(h, i); % Compute e_k' * GMAT * e_i
+                term3 = GMAT(j, k); % Compute e_j' * GMAT * e_h
+                term4 = INV1(h, i); % Compute e_k' * INV1 * e_i
+                H(p,q) = (sqp(i)/sqp(j))*...
+                    (sqp(h)/sqp(k))*(term1 * term2 + term3 * term4);
+            end
+        end
+    end
+end
+
+H = H + speye(n^2,n^2);
+
+end
+
+function H = assemble_H_mex(Delta,lambda,P,pi)
+%ASSEMBLE_H builds the Hessian for the Kemeny's constant function
+
+n = size(P,1);
+I = eye(n,n);
+Delta = reshape(Delta,n,n);
+sqp = sqrt(pi);
+Dp = spdiags(sqp,0,n,n);
+Dpinv = spdiags(1./sqp,0,n,n);
+L = chol(I - Dp*(P+Delta)*Dpinv + sqp*sqp');
+INV1 = L'\(L\I);
+GMAT = INV1*INV1;
+
+% Define the dimension n
+n = size(INV1, 1);
+
+H = assemble_hessian(INV1,GMAT,n);
+
+end
+
 
 %% Utilities
 % These function are used to build the matrices of the constraints for the
